d444cf260937783025850d3b74f7fe4efa97feb6
Ryan Mooney <wm.ryan.mooney@gmail.com>
2023-01-17 18:42:18 -0900

    Get the Word Count of the book

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
-    print(file_contents)
+book = "books/frankenstein.txt"
+book_name = "Frankenstein"
+
+
+def main():
+    text = get_book(book)
+    w_count = word_count(text)
+    print(f"{w_count} words were found in {book_name}")
+
+
+def word_count(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book(file):
+    with open(book) as b:
+        return b.read()
+
+
+main()
