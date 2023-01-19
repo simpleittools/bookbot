a2fedd0436585346c227f108cc00fafaae88897a
Ryan Mooney <wm.ryan.mooney@gmail.com>
2023-01-18 16:09:16 -0900

    Count per letter

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
     text = get_book(book)
     w_count = word_count(text)
     print(f"{w_count} words were found in {book_name}")
+    chars_dict = get_chars_dict(text)
+    print(chars_dict)
 
 
 def word_count(text):
@@ -16,6 +18,19 @@ def word_count(text):
 def get_book(file):
     with open(book) as b:
         return b.read()
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
 
 
 main()
