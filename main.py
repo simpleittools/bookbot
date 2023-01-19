c64bafbf5d4ad4158350955a1887e100f31c7bbf
Ryan Mooney <wm.ryan.mooney@gmail.com>
2023-01-18 16:22:56 -0900

    Sorted count per letter

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ def main():
     print(f"{w_count} words were found in {book_name}")
     chars_dict = get_chars_dict(text)
     print(chars_dict)
+    c_sorted_list = c_to_sort(chars_dict)
+
+    print(f"--- Begin report of {book} ---")
+    print(f"{word_count} words found in the document")
+    print()
+
+    for item in c_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
 
 
 def word_count(text):
@@ -31,6 +43,16 @@ def get_chars_dict(text):
     return chars
 
 
+def sort_on(d):
+    return d["num"]
+
+
+def c_to_sort(num_c_dict):
+    sorted_list = []
+    for c in num_c_dict:
+        sorted_list.append({"char": c, "num": num_c_dict[c]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 
 main()
